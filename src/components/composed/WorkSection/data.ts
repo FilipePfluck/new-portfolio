fd562ab92876f258b055beb3dff5bfca7361ac97
Filipe Pfluck <filipepfluckdev@gmail.com>
2023-09-19 18:23:03 -0300

    changing text in work section

--- src/components/composed/WorkSection/data.ts
+++ src/components/composed/WorkSection/data.ts
@@ -2,31 +2,43 @@ export const workExperiences = [
   {
     description:
       'Vobi is a project management platform for small businesses in the residential construction industry, revolutionazing one of the less digitized industries in Brazil. Vobi was selected for a batch on YCombinator on 2022. It empowers designers and architects by providing them tools to manage their business, control their finances and much more. I worked as a fullstack developer for Vobi, mantaining and adding new features to the platform built with Node and React.',
-    duration: 'Set. 2021 - Ago. 2022',
+    duration: 'Set. 2021 - Aug. 2022',
     name: 'Vobi',
     src: '/company/vobi.jpg',
+    topics: [
+      {
+        name: 'Improvement mindset',
+        description:
+          'I always gave feedback about the new features and tried to find ways to improve our code and our product. ',
+      },
+      {
+        name: 'Important features',
+        description:
+          'I delivered important features, such as a schedule, one of the features most loved by the users.',
+      },
+    ],
   },
   {
     description:
-      'Dexper is a company that builds platforms for enterprises to host online events available across the world. Once the live event has finished, the content is automatically available on-demand. Dexper works with a whitelabel product that is than tailored and completely customized to meet the client company branding. This is done by storing the theme on a firestore document. The frontend is built with Nextjs and ChakraUI. While I worked for Dexper, I had the opportunity to help building huge events',
+      'Dexper is a company that builds platforms for enterprises to host online events available across the world. Once the live event has finished, the content is automatically available on-demand. Dexper works with a whitelabel product that is than tailored and completely customized to meet the client company branding.',
     duration: 'Feb. 2023 - Jul. 2023',
     name: 'Dexper',
     src: '/company/dexper.jpeg',
     topics: [
       {
+        name: 'Global events',
         description:
-          "Config is Figma's annual design conference. This year it was hosted on 21st and 22nd of June on San Francisco, and virtually on the platform built by Dexper.",
-        name: 'Config 2023',
+          "While I worked for Dexper, I had the opportunity to help developing platforms to host huge events. The most exciting was Config 2023, Figma's annual design conference. This year it was hosted on San Francisco, and virtually on the platform built by Dexper. More than 40k people watched the event.",
       },
       {
+        name: 'Learning culture',
         description:
-          'Smartcon is a conference organized by Chainlink focused on Web3. It will happen on October 2-3 on Barcelona.',
-        name: 'Smartcon 2023',
+          'I Started a culture of sharing knowledge in the company, by doing recurring meetings between developers. I taught my coworkers about the importance of accessibility and how to meet WAI-ARIA criteria.',
       },
       {
+        name: 'Web Core Vitals',
         description:
-          'Remote is a company that helps hiring people around the world. This event is for their global audience.',
-        name: 'Remote Connect 2023',
+          'I Improved the web core vitals scores on several pages across multiple projects, including performance and accessibility. Some of the improvements I did include faster font loading, reduction of Cumulative Layout Shift and patching of several accessibility issues.',
       },
     ],
   },
